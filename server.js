0d23f83c363bf7d61e305080879f7e32db8e8b52
Sami Hamed <sami.k.hamed@googlemail.com>
2019-05-31 22:22:11 +0200

    :tada: Initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+app.get('/', function(_req, res) {
+    res.sendFile(path.join(__dirname, '../index.html'))
+})
+
+app.listen(process.env.PORT || 4000, function(){
+    console.log(`Your node js server is running on port ${process.env.PORT || 4000}`)
+})
